17b5790d944c5421eda1e78d7ade6377767cda62
Leo-Dan <leo-daniel.gosselin.1@ens.etsmtl.ca>
2022-10-25 16:15:12 -0400

    merged on and off services into one "toggle" service

--- src/gpio_control.cpp
+++ src/gpio_control.cpp
@@ -7,43 +7,52 @@
 const jetsonXavierGPIONumber WRIST_LIGHT_PIN = jetsonXavierGPIONumber::gpio352;
 const jetsonXavierGPIONumber BACK_LIGHT_PIN = jetsonXavierGPIONumber::gpio353;
 const int ros_spin_rate = 100;
+bool wrist_light_state=off;
+bool back_light_state=off;
 
 
-
-bool wristLightOn(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
+bool wrist_light_toggle(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
 {
-    res.message = "successfully turned wrist_light on";
-    gpioSetValue(WRIST_LIGHT_PIN, 1);
-    res.success = static_cast<unsigned char>(true);
-    return true;
+    if(wrist_light_state==off)
+    {
+        gpioSetValue(WRIST_LIGHT_PIN, on);
+        wrist_light_state=on;
+        res.message = "successfully turned wrist_light on";
+        res.success = static_cast<unsigned char>(true);
+        return true;
+    }
+    else
+    {
+        gpioSetValue(WRIST_LIGHT_PIN, off);
+        wrist_light_state=off;
+        res.message = "successfully turned wrist_light off";
+        res.success = static_cast<unsigned char>(true);
+        return true;
+    }
 }
 
 
-bool wristLightOff(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
+
+bool back_light_toggle(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
 {
-    res.message = "successfully turned wrist_light off";
-    gpioSetValue(WRIST_LIGHT_PIN, 0);
-    res.success = static_cast<unsigned char>(true);
-    return true;
+   if(back_light_state==off)
+    {
+        gpioSetValue(BACK_LIGHT_PIN, on);
+        back_light_state=on;
+        res.message = "successfully turned back_light on";
+        res.success = static_cast<unsigned char>(true);
+        return true;
+    }
+    else
+    {
+        gpioSetValue(BACK_LIGHT_PIN, off);
+        back_light_state=off;
+        res.message = "successfully turned back_light off";
+        res.success = static_cast<unsigned char>(true);
+        return true;
+    }
 }
 
-
-bool backLightOn(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
-{
-    res.message = "successfully turned back_light on";
-    gpioSetValue(BACK_LIGHT_PIN, 1);
-    res.success = static_cast<unsigned char>(true);
-    return true;
-}
-
-
-bool backLightOff(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
-{
-    res.message = "successfully turned back_light off";
-    gpioSetValue(BACK_LIGHT_PIN, 0);
-    res.success = static_cast<unsigned char>(true);
-    return true;
-}
 
 /**
  * Initialize the configuration to control the GPIO pin.
@@ -52,10 +61,10 @@ void initializeGPIO()
 {
     ROS_INFO("Succesfully initialized gpio's");
     gpioSetDirection(WRIST_LIGHT_PIN,outputPin);
-    gpioSetValue(WRIST_LIGHT_PIN, 0);
+    gpioSetValue(WRIST_LIGHT_PIN, off);
 
     gpioSetDirection(BACK_LIGHT_PIN,outputPin);
-    gpioSetValue(BACK_LIGHT_PIN, 0);
+    gpioSetValue(BACK_LIGHT_PIN, off);
 
 }
 
@@ -66,11 +75,9 @@ int main(int argc, char **argv)
 
     initializeGPIO();
 
-    ros::ServiceServer wristLightEnable = nh.advertiseService("wristLightOn", wristLightOn);
-    ros::ServiceServer wristLightDisable = nh.advertiseService("wristLightOff", wristLightOff);
+    ros::ServiceServer wristLightEnable = nh.advertiseService("wrist_light_toggle", wrist_light_toggle);
 
-    ros::ServiceServer backLightEnable = nh.advertiseService("backLightOn", backLightOn);
-    ros::ServiceServer backLightDisable = nh.advertiseService("backLightOff", backLightOff);
+    ros::ServiceServer backLightEnable = nh.advertiseService("back_light_toggle", back_light_toggle);
     
     ros::Rate rate(ros_spin_rate); // ROS Rate at 100Hz
 
