7d93f36a31e739ac512dc4c48753fe008aea77b6
Leo-Dan <leo-daniel.gosselin.1@ens.etsmtl.ca>
2023-02-03 16:56:16 -0500

    WIP-added some code modularity

--- src/gpio_control.cpp
+++ src/gpio_control.cpp
@@ -2,17 +2,17 @@
 #include "std_srvs/Trigger.h"
 #include "JetsonXavierGPIO/jetsonGPIO.h"
 
-const jetsonXavierGPIONumber WRIST_LIGHT_PIN = jetsonXavierGPIONumber::gpio352;
-const jetsonXavierGPIONumber BACK_LIGHT_PIN = jetsonXavierGPIONumber::gpio353;
+// const jetsonXavierGPIONumber WRIST_LIGHT_PIN = jetsonXavierGPIONumber::gpio352;
+// const jetsonXavierGPIONumber BACK_LIGHT_PIN = jetsonXavierGPIONumber::gpio353;
 const int ROS_SPIN_RATE = 100;
 bool wrist_light_state = pinValues::off;
 bool back_light_state = pinValues::off;
 
-bool wrist_light_toggle(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res)
+bool gpio_toggle(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res, jetsonXavierGPIONumber pin_num);
 {
   if (wrist_light_state == pinValues::off)
   {
-    gpioSetValue(WRIST_LIGHT_PIN, pinValues::on);
+    gpioSetValue(pin_num, pinValues::on);
     wrist_light_state = pinValues::on;
     res.message = "successfully turned wrist_light on";
     res.success = static_cast<unsigned char>(true);
@@ -20,29 +20,9 @@ bool wrist_light_toggle(std_srvs::Trigger::Request& req, std_srvs::Trigger::Resp
   }
   else
   {
-    gpioSetValue(WRIST_LIGHT_PIN, pinValues::off);
+    gpioSetValue(pin_num, pinValues::off);
     wrist_light_state = pinValues::off;
     res.message = "successfully turned wrist_light off";
-    res.success = static_cast<unsigned char>(true);
-    return true;
-  }
-}
-
-bool back_light_toggle(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res)
-{
-  if (back_light_state == pinValues::off)
-  {
-    gpioSetValue(BACK_LIGHT_PIN, pinValues::on);
-    back_light_state = pinValues::on;
-    res.message = "successfully turned back_light on";
-    res.success = static_cast<unsigned char>(true);
-    return true;
-  }
-  else
-  {
-    gpioSetValue(BACK_LIGHT_PIN, pinValues::off);
-    back_light_state = pinValues::off;
-    res.message = "successfully turned back_light off";
     res.success = static_cast<unsigned char>(true);
     return true;
   }
@@ -51,25 +31,36 @@ bool back_light_toggle(std_srvs::Trigger::Request& req, std_srvs::Trigger::Respo
 /**
  * Initialize the configuration to control the GPIO pin.
  */
-void initializeGPIO()
+void initializeGPIO(jetsonXavierGPIONumber pin_num)
 {
-  gpioExport(WRIST_LIGHT_PIN);
-  gpioSetDirection(WRIST_LIGHT_PIN, pinDirections::outputPin);
-  gpioSetValue(WRIST_LIGHT_PIN, pinValues::off);
+  gpioExport(pin_num);
 
-  gpioExport(BACK_LIGHT_PIN);
-  gpioSetDirection(BACK_LIGHT_PIN, pinDirections::outputPin);
-  gpioSetValue(BACK_LIGHT_PIN, pinValues::off);
-
+  // si c'est un output
+  gpioSetDirection(pin_num, pinDirections::outputPin);
+  gpioSetValue(pin_num, pinValues::off);
   ROS_INFO("Succesfully initialized gpio's");
+  // sinon si cest un input
+  gpioSetDirection(pin_num, pinDirections::inputPin);
+  ROS_INFO("Succesfully initialized gpio's");
+
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "capra_gpio_control");
   ros::NodeHandle nh;
+  int int_var;
+  double double_var;
+  std::string string_var;
 
-  initializeGPIO();
+  ros::param::get("/my_integer", int_var);
+  ros::param::get("/my_float", double_var);
+  ros::param::get("/my_string", string_var);
+
+  ROS_INFO("Int: %d, Float: %lf, String: %s", int_var, double_var, string_var.c_str());
+
+  int used_pin = jetsonXavierGPIONumber::gpio352;
+  initializeGPIO((jetsonXavierGPIONumber)used_pin);
 
   ros::ServiceServer wristLightEnable = nh.advertiseService("wrist_light_toggle", wrist_light_toggle);
 
